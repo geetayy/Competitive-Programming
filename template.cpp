7b263993de7876c67a6faa1e1c6ef1e5e30210a6
Rahul Mishra <rm983845@gmail.com>
2025-03-19 16:19:20 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <climits>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <cmath>
+using namespace std;
+#define f first
+#define s second
+#define int long long int
+// const int N = 1e6 ;
+// vector<int> a;
+
+void solve() {
+}
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
